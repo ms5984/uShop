60c7967f10e9d6fd287b350d7b832f7308d6c212
Matt <58913953+ms5984@users.noreply.github.com>
2021-07-30 16:46:28 -0400

    Simplify handler * Remove event, not needed * Sell handler logic stored in CommandOnSellHandler#processCommand; configurable * Update Listeners (remove event + change method call) * Add on-sell section to config.yml

--- src/main/java/xyz/spaceio/ushop/Listeners.java
+++ src/main/java/xyz/spaceio/ushop/Listeners.java
@@ -26,9 +26,6 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import xyz.spaceio.customitem.CustomItem;
-// begin sale event addition ms5984
-import xyz.spaceio.ushop.events.UShopSellEvent;
-// end sale event addition ms5984
 
 
 public class Listeners implements Listener {
@@ -88,9 +85,9 @@ public class Listeners implements Listener {
 					p.sendMessage(plugin.getConfig().getString("message-sold").replace('&', '§').replace("%total%", plugin.getEconomy().format(total)));
 					
 					HashMap<CustomItem, Integer> listOfItems = plugin.getSalableItems(e.getInventory().getContents());
-					// begin sale event addition ms5984
-					plugin.getServer().getPluginManager().callEvent(new UShopSellEvent(p, listOfItems));
-					// end sale event addition ms5984
+					// begin command on sell addition ms5984
+					new CommandOnSellHandler(p, listOfItems).processCommand();
+					// end command on sell addition ms5984
 					List<String> allLines = new ArrayList<>();
 					allLines.add(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.header")));
 					for (Entry<CustomItem, Integer> entry : listOfItems.entrySet()) {
